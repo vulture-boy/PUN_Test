10705b00c09be6d3495455c72a1d2e307bb797c2
vvv <t.moll@outlook.com>
2025-03-26 22:25:36 -0400

    Completed tutorial. Ready for sample project creation.

--- Assets/Tennis/Scripts/PlayerManager.cs
+++ Assets/Tennis/Scripts/PlayerManager.cs
@@ -19,6 +19,13 @@ namespace com.vvv.tennis
         [Tooltip("The current Health of our player")]
         public float Health = 1f;
 
+        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+
+        [Tooltip("The Player's UI GameObject Prefab")]
+        [SerializeField]
+        public GameObject PlayerUiPrefab;
+
         #endregion
 
         #region Private Fields
@@ -26,6 +33,7 @@ namespace com.vvv.tennis
         [Tooltip("The Beams GameObject to control")]
         [SerializeField]
         private GameObject beams;
+
         //True, when the user is firing
         bool IsFiring;
         #endregion
@@ -57,6 +65,16 @@ namespace com.vvv.tennis
         /// </summary>
         void Awake()
         {
+            // #Important
+            // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+            if (photonView.IsMine)
+            {
+                PlayerManager.LocalPlayerInstance = this.gameObject;
+            }
+            // #Critical
+            // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+            DontDestroyOnLoad(this.gameObject);
+
             if (beams == null)
             {
                 Debug.LogError("<Color=Red><a>Missing</a></Color> Beams Reference.", this);
@@ -85,6 +103,21 @@ namespace com.vvv.tennis
             {
                 Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
             }
+
+            if (PlayerUiPrefab != null)
+            {
+                GameObject _uiGo = Instantiate(PlayerUiPrefab);
+                _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+            }
+            else
+            {
+                Debug.LogWarning("<Color=Red><a>Missing</a></Color> PlayerUiPrefab reference on player Prefab.", this);
+            }
+
+#if UNITY_5_4_OR_NEWER
+            // Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+#endif
         }
 
         /// <summary>
@@ -151,6 +184,35 @@ namespace com.vvv.tennis
             Health -= 0.1f * Time.deltaTime;
         }
 
+#if UNITY_5_4_OR_NEWER
+        public override void OnDisable()
+        {
+            // Always call the base to remove callbacks
+            base.OnDisable();
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+#endif
+
+#if !UNITY_5_4_OR_NEWER
+        /// <summary>See CalledOnLevelWasLoaded. Outdated in Unity 5.4.</summary>
+        void OnLevelWasLoaded(int level)
+        {
+            this.CalledOnLevelWasLoaded(level);
+        }
+#endif
+
+        void CalledOnLevelWasLoaded(int level)
+        {
+            // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
+            if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
+            {
+                transform.position = new Vector3(0f, 5f, 0f);
+            }
+
+            GameObject _uiGo = Instantiate(this.PlayerUiPrefab);
+            _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+        }
+
         #endregion
 
         #region Private Methods
@@ -175,6 +237,13 @@ namespace com.vvv.tennis
                 }
             }
         }
+
+#if UNITY_5_4_OR_NEWER
+        void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode loadingMode)
+        {
+            this.CalledOnLevelWasLoaded(scene.buildIndex);
+        }
+#endif
 
         #endregion
     }
